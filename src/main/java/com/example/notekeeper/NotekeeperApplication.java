04097610ab27a0cc0e1231a34cb1b0be8e298b20
Sergej Steinsiek <sergejsteinsiek@proton.me>
2024-09-26 08:43:37 +0200

    initial commit

---
+++ src/main/java/com/example/notekeeper/NotekeeperApplication.java
@@ -0,0 +1,13 @@
+package com.example.notekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotekeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notekeeper/NotekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
