d23644c9d11e32823837fe8d20c803aa3cf4756b
Sergej Steinsiek <sergejsteinsiek@proton.me>
2024-09-27 09:25:23 +0200

    Basic Operations /notes/

--- src/main/java/com/example/notekeeper/NoteController.java
+++ src/main/java/com/example/notekeeper/NoteController.java
@@ -1,10 +1,17 @@
 package com.example.notekeeper;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
 
 
 @RestController
@@ -12,12 +19,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class NoteController {
     private final NoteService service;
 
-    public NoteController(NoteService service) {            
+    public NoteController(NoteService service) {
         this.service = service;
     }
-    
-    @GetMapping
-    public List<Note> getAll() {
-        return service.getAll();                                        
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> get(@PathParam("id") UUID id) {
+        Note result = service.get(id);
+
+        if(result == null)
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Note>> getAll() {
+        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Note> post(@RequestBody Note note) {
+        Note result = service.post(note);
+
+        if(result == null)
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        else
+            return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/example/notekeeper/NoteService.java
+++ src/main/java/com/example/notekeeper/NoteService.java
@@ -2,15 +2,27 @@ package com.example.notekeeper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class NoteService {
+    public Note get(UUID id)
+    {
+        return new Note();            
+    }
+
     public List<Note> getAll()
     {
         List<Note> notelist = new ArrayList<>();
         notelist.add(new Note());
+
         return notelist;
+    }
+
+    public Note post(Note note)
+    {
+        return note;
     }
 }
