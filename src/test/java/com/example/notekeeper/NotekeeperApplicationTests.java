1ba49eb3f68179e1d655177701752d91efef3c3f
Christoph <kissmannchristoph@gmail.com>
2024-11-07 22:18:19 +0100

    add first test

--- src/test/java/com/example/notekeeper/NotekeeperApplicationTests.java
+++ src/test/java/com/example/notekeeper/NotekeeperApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.notekeeper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NotekeeperApplicationTests {
 
+	private NoteService noteService = new NoteService();
+
 	@Test
-	void contextLoads() {
+	void testAddNote() {
+		Note note = new Note();
+		
+		noteService.post(note);
+
+		assertEquals(1, noteService.getAll().size());
 	}
 
 }
