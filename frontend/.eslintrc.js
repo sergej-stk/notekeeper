097547344438cca26fcc021a5c355d90d627a562
Christoph <kissmannchristoph@gmail.com>
2024-11-22 22:56:00 +0100

    fix: ignore protofiles in eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -16,5 +16,5 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
-  ignores: ["src/ts/proto/*"],
+  ignorePatterns: ["src/ts/proto/*"],
 };
