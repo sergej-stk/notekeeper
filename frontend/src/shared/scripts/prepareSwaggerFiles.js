f7b7d22349d99c86115a673d393bb49c1f6d91a8
Christoph <kissmannchristoph@gmail.com>
2024-11-27 19:58:10 +0100

    fix: fixes

--- frontend/src/shared/scripts/prepareSwaggerFiles.js
+++ frontend/src/shared/scripts/prepareSwaggerFiles.js
@@ -1,25 +1,33 @@
 import fs from "fs";
 import { execSync } from "child_process";
 
-function getExecString(file) {
-    return "asyncapi generate fromTemplate .\\" +file + " @asyncapi/html-template@2.3.14 --param version=3.0.0 singleFile=true --output ./docs --force-write"
-} 
-  
 const swaggerPath = "../docs/swagger";
 const asyncApiPath = "../docs/asyncapi";
+
+function getExecString(file) {
+    return `asyncapi generate fromTemplate ${asyncApiPath}/${file} @asyncapi/html-template@2.3.14 --param version=2.2.0 singleFile=false --output ${asyncApiPath}/docs/${file} --force-write`;
+} 
 
 const swaggerFiles = fs.readdirSync(swaggerPath);
 const asyncApiFiles = fs.readdirSync(asyncApiPath).filter(search => search !== "docs");
 
+for (const asyncApiFile of asyncApiFiles) {
+    console.log("build asyncapi: " + asyncApiFile);
 try {
-    const cmd = 'git rev-parse --is-inside-work-tree';
-    execSync(cmd).toString();
+    const cmd = getExecString(asyncApiFiles[0]);
+    const out = execSync(cmd).toString();
+
+   // await fs.rename(`${asyncApiPath}/../docs/index.html`, `${asyncApiPath}/../docs/${file}`);
+    console.log(out);
  } catch (error) {
+    console.error("error build asyncapi: " + asyncApiFile);
+    console.error(error.message);
     error.status;  // 0 : successful exit, but here in exception it has to be greater than 0
     error.message; // Holds the message you typically want.
     error.stderr;  // Holds the stderr output. Use `.toString()`.
     error.stdout;  // Holds the stdout output. Use `.toString()`.
  }
+}
 
 fs.writeFileSync("../docs/list.json", JSON.stringify({
     swaggerFiles,
